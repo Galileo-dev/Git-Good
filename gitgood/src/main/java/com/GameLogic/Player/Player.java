6048d60e511cffebfcaaf049b391f32cf573b47a
conorglynn02 <conorglynn02@gmail.com>
2022-12-02 14:17:33 +0000

    Conor Player v1

--- gitgood/src/main/java/com/GameLogic/Player/Player.java
+++ gitgood/src/main/java/com/GameLogic/Player/Player.java
@@ -2,4 +2,41 @@ package com.GameLogic.Player;
 
 public class Player {
     
+    private int lifeCount;
+
+    //constructor
+    public Player(int lifeCount) {
+
+        this.lifeCount = lifeCount;
+
+    }
+
+    public int getLifeCount() {
+        return lifeCount;
+    }
+
+    public void setLifeCount(int lifeCount) {
+        this.lifeCount = lifeCount;
+    }
+
+    //lose life method
+    public void loseLife() {
+
+        this.lifeCount--;
+        restart();
+
+        if (this.lifeCount <= 0) {
+            endgame();
+        }
+    }
+
+    //restart game method
+    public void restart() {
+
+        //clearscreen();
+    }
+
+    public void endgame() {
+        this.lifeCount = 5;
+    }
 }
