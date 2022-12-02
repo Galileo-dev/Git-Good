ac01f4a6dd368f1d18c40186460a7001341b5679
oisinosullivan9 <oisinosull@gmail.com>
2022-12-02 14:19:21 +0000

    created Start class v1

---
+++ gitgood/src/main/java/com/GameLogic/start/Start.java
@@ -0,0 +1,27 @@
+package com.GameLogic.start;
+
+public class Start {
+    
+    private int levelChoice;
+
+    public Start(int levelChoice) {
+
+        this.levelChoice = levelChoice;
+    }
+
+    public int getLevelChoice() {
+        return levelChoice;
+    }
+
+    public void setLevelChoice(int levelChoice) {
+        this.levelChoice = levelChoice;
+    }
+    
+    public void startLevel() {
+
+        //level1();...
+        
+
+    }
+    } 
+
