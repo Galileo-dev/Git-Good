38353399c82a81a96ff321d958a769ae126e09da
Galileo-dev <fionn2200@gmail.com>
2022-12-08 11:38:00 +0000

    fixed level selection

--- gitgood/src/main/java/com/gitgood/game/levels/LevelLogic.java
+++ gitgood/src/main/java/com/gitgood/game/levels/LevelLogic.java
@@ -1,13 +1,28 @@
 package com.gitgood.game.levels;
 
+import java.util.Map;
+
 public abstract class LevelLogic implements Runnable {
     public String command;
     public boolean isFinished = false;
+    public Map<String, String> config;;
+
+    public LevelLogic(Map<String, String> config) {
+        this.config = config;
+        // run the function that was passed in
+    }
+
+    public LevelLogic() {
+    }
 
     public void run(String command) {
         this.command = command;
         // run the function that was passed in
         run();
+
+    }
+
+    public void run() {
 
     }
 }
